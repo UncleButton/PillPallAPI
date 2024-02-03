d99aa5c48c91c57455e44200717b733cd90cc0e2
jdfel <justinfeldmann01@gmail.com>
2024-02-03 15:45:11 -0500

    first test

--- Controllers/DispenseController.cs
+++ Controllers/DispenseController.cs
@@ -1,5 +1,7 @@
 //Britton Bailer, January 2024
 
+using System;
+using System.IO.Ports;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PillPallAPI.Controllers;
@@ -22,6 +24,36 @@ public class DispenseController : ControllerBase
         //JUSTIN'S AREA
 
         //PUT LOGIC HERE THAT DOES THE ACTUAL DISPENSING/USES YOUR DEVICE CONTROLLERS
+        
+       SerialPort sp = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
+
+            // Set the read timeout to 500 ms
+            sp.ReadTimeout = 500;
+
+            // Open the port
+            sp.Open();
+
+            // Read data from the port until the user presses a key
+            Console.WriteLine("Reading data from GPS device...");
+            Console.WriteLine("Press any key to stop.");
+            while (!Console.KeyAvailable)
+            {
+                try
+                {
+                    // Read a line of data
+                    string data = sp.ReadLine();
+
+                    // Display the data
+                    Console.WriteLine(data);
+                }
+                catch (TimeoutException)
+                {
+                    // Ignore timeout errors
+                }
+            }
+
+            // Close the port
+            sp.Close();
 
         return Ok();
     }
