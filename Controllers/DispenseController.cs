3ef4703871cd580262abfc57571b259741f16ebd
jdfel <justinfeldmann01@gmail.com>
2024-02-09 19:09:58 -0500

    sending arrays works

--- Controllers/DispenseController.cs
+++ Controllers/DispenseController.cs
@@ -25,60 +25,60 @@ public class DispenseController : ControllerBase
 
         //PUT LOGIC HERE THAT DOES THE ACTUAL DISPENSING/USES YOUR DEVICE CONTROLLERS
 
-       SerialPort sp = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
+        SerialPort sp = new SerialPort("COM7", 9600, Parity.None, 8, StopBits.One);
 
-            // Set the read timeout to 500 ms
-            sp.ReadTimeout = 5000;
+        // Set the read timeout to 5 s
+        sp.ReadTimeout = 5000;
 
-            // Open the port
-            sp.Open();
+        // Open the port
+        sp.Open();
 
-            // Read data from the port until the user presses a key
-            Console.WriteLine("Reading data from GPS device...");
-            Console.WriteLine("Press any key to stop.");
-            while (!Console.KeyAvailable)
+        // Array with preset values is sent to the arduino, which sums the values and writes back the result
+        byte[] disps = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        while (!Console.KeyAvailable)
+        {
+            try
             {
-                try
-                {
-                    Console.WriteLine("Trying to Read...");
-                    // Read a line of data
-                    sp.WriteLine("5");
-                    string data = sp.ReadLine();
+                Console.WriteLine("Trying to Write...");
+                // Write the array to the arduino via the serial port. "disps" is the array of bytes, 0 is the offset (leave at 0), and 10 is the amount of bytes
+                sp.Write(disps, 0, 10);
+                // Read the result back from the arduino
+                string data = sp.ReadLine();
 
-                    // Display the data
-                    Console.WriteLine(data);
-
-                }
-                catch (TimeoutException)
-                {
-                    // Ignore timeout errors
-                }
+                // Display the data
+                Console.WriteLine(data);
             }
+            catch (TimeoutException)
+            {
+                // Ignore timeout errors
+            }
+        }
 
-            // Close the port
-            sp.Close();
+        // Close the port
+        sp.Close();
 
         return Ok();
     }
-
-    // This is the code the arduino had at the time of success...
-
-    // void setup() {
-    //   Serial.begin(9600);
-
-    // }
-
-    // void loop() {
-    //   if (Serial.available())
-    //   {
-    //     int num = Serial.read()
-    //     // String str = Serial.readStringUntil(' ');
-    //     Serial.println(str);
-    //     // if (str == "5")
-    //     //   Serial.println("Message DOUBLE received");
-    //   }
-    //   // Serial.println("Talking from Arduino");
-    //   delay(1000);
-    // }
-
 }
+
+// This is the code the arduino had at the time of success...
+
+// void setup()
+// {
+//   delay(3000);
+//   Serial.begin(9600);
+// }
+
+// void loop()
+// {
+//     if (Serial.available())
+//     {
+//         int sum = 0;
+//         for (int i = 0; i < 10; i++)
+//         {
+//             sum += Serial.read();
+//         }
+//         Serial.println(sum);
+//     }
+//     delay(1000);
+// }
